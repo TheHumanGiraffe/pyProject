daca2976b1e7f4910f0d60ed7d5aaedf836110f1
mhham1227 <mhham592@gmail.com>
2019-04-25 10:05:36 -0500

    Pre Cleanup

--- src/apps/Interpreter.java
+++ src/apps/Interpreter.java
@@ -1,146 +1,247 @@
 package apps;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;  // Import the Scanner class
 
-/**
- * 
- * Interpreter.java
- * 
- * Asks for input in the console, and then runs Tokenizer, SimpleParser, and Evaluator
- * to return the result.
- * 
- * @author Mathias Ham
- * @author Micheal Walburn
- * @author Morgan Patterson
- * 
- */
+import static apps.Constants.REGEXVAR;
+
+
 public class Interpreter {
 
+	private static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		HashMap<String, String> map = new HashMap<>(); 
+		HashMap<String, String> varMap = new HashMap<>(); 
+		HashMap<String, InstructionContent> functionMap = new HashMap<>();
 		String input = null;
-		Scanner scan = new Scanner(System.in);
-		Evaluator eval = new Evaluator();
-		while(input != "quit"){
-			input = scan.nextLine();
-			if(checkForVars(input, map)){
-				input = updateInput(input, map);
-				eval.eval(input);
-			}
-			else{
-				eval.eval(input);
-			}
-		}
-		scan.close();
-	}
-
-	/**
-	 * If the given string has a variable, look for it in the HashMap.
-	 * @param input - A given String
-	 * @param map - A set of all defined variables for the current Interpreter session
-	 * @return An updated string with the variables replaced with their values.
-	 */
-	private static String updateInput(String input, Map<String, String> map) {
 		
 		String[] tokens;
-		String regexVar = "[a-zA-Z_]\\w+|[a-df-zA-DF-Z_]\\w*";
-
-		Tokenizer tokenizer = new Tokenizer();
-		tokens = tokenizer.generateTokens(input);
 		
-		for(int i = 0; i < tokens.length; i++){
-			if(tokens[i].matches(regexVar)){
-				if(map.containsKey(tokens[i])){
-					tokens[i] = map.get(tokens[i]);
+		//This keeps from saying that "print" is not a assigned Var when using the print() function
+		varMap.put("print", "");
+		/*
+  ___      _   _                   _   __  
+ | _ \_  _| |_| |_  ___ _ _    ___/ | /  \ 
+ |  _/ || |  _| ' \/ _ \ ' \  |___| || () |
+ |_|  \_, |\__|_||_\___/_||_|     |_(_)__/ 
+      |__/                                 
+
+		 */	
+		System.out.println("  ___      _   _                   _   __ \n"+
+							" | _ \\_  _| |_| |_  ___ _ _    ___/ | /  \\ \n"+
+							" |  _/ || |  _| ' \\/ _ \\ ' \\  |___| || () |\n"+
+							" |_|  \\_, |\\__|_||_\\___/_||_|     |_(_)__/ \n"+
+									"     |__/                                 \n");	
+		while(true){
+			System.out.printf("\n>>>");
+			input = scan.nextLine();
+			tokens = Tokenizer.tokenize(input);
+			if(SimpleParser.isForLoop(input)){
+				HashMap<String, InstructionContent> blockInstructionsMap = generateBlockMap();
+				 generateForLoop(input, varMap, blockInstructionsMap);
+				 Evaluator.runBlock(blockInstructionsMap, input, varMap);
+				continue;
+			}
+			
+			if(SimpleParser.isIf(input)){
+				HashMap<String, InstructionContent> blockInstructionsMap = generateBlockMap();
+				generateIf(input, varMap, blockInstructionsMap);	
+				 Evaluator.runBlock(blockInstructionsMap, input,  varMap);
+				continue;
+			}
+			if(tokens.length > 2){
+				if(SimpleParser.isFunctionDef(tokens[0], tokens[2])){
+					generateFunction(input,varMap, functionMap);
+					continue;
 				}
-				
+				if(SimpleParser.isFunctionCall(input)){
+					tokens[2] =Evaluator.updateInput(input, varMap, tokens[2].split("((?<=,)|(?=,))"));
+					input = arrayToString(tokens, 0, tokens.length);
+					Evaluator.runBlock(functionMap, input,  varMap);
+					continue;
+				}
 			}
-		
+			
+			if(input.equals("quit")){
+				System.out.println("Goodbye. Hope you had fun!");
+				break;
 			}
-		
-		return arrayToString(tokens, 0, tokens.length);
-		
-	}
-
-	/**
-	 * Converts the given tokenized array to a String
-	 * @param array - Array to Convert
-	 * @param start - Starting index to convert
-	 * @param end - ending index to convert
-	 * @return a String 'str'
-	 */
-	private static String arrayToString(String[] array, int start, int end){
-		StringBuilder builder = new StringBuilder();
-		for (int i =start; i < end; i++){
-			builder.append(array[i]);
+			interpret(input, tokens, varMap, null );
 		}
-		
-		String str = builder.toString();
-		return str;	
 	}
 	
-	/**
-	 * Checks if a variable has been created in the HashMap
-	 * @param input - String to be tested.
-	 * @param map - The HashMap
-	 * @return If the variable has been created return true, if not, false.
-	 */
-	private static boolean checkForVars(String input, Map<String, String> map) {
-		String[] tokens;
-		String regexVar = "[a-zA-Z_]\\w+|[a-df-zA-DF-Z_]\\w*";
-		Evaluator eval = new Evaluator();
-		Tokenizer tokenizer = new Tokenizer();
-		tokens = tokenizer.generateTokens(input);
+	public static void interpret(String input, String[] tokens,  Map<String, String> map, HashMap<String, InstructionContent> blockInstructionsMap){
+		if(SimpleParser.isForLoop(input)|| SimpleParser.isIf(input) ){
+			Evaluator.runBlock(blockInstructionsMap, input,  map);
+		}
+		if(tokens.length > 2){
+			if(SimpleParser.isFunctionCall(input)){
+				tokens[2] = Evaluator.updateInput(input, map, tokens[2].split("((?<=,)|(?=,))"));
+				input = arrayToString(tokens, 0, tokens.length);
+				Evaluator.runBlock(blockInstructionsMap, input,  map);
+			}
+		}
+		if(SimpleParser.isPrint(input)){
+			StringBuilder builder = new StringBuilder();
+			for(int i = input.indexOf('(')+1; i < input.lastIndexOf(')'); i++){
+				builder.append(input.charAt(i));
+			}
+			String[] printTokens;
+			String whatToPrint= builder.toString();
+			printTokens = Tokenizer.tokenize(whatToPrint);
+			interpret(whatToPrint,  printTokens, map, blockInstructionsMap);
+			
+			return;
+		}
 		
+		
+		if(checkForVars(input, map, tokens)){
+			input = Evaluator.updateInput(input, map, tokens);
+			print(Evaluator.eval(input));
+		}
+		else{
+			print(Evaluator.eval(input));
+		}
+	}
+
+	protected static HashMap<String, InstructionContent> generateBlockMap(){
+		HashMap<String, InstructionContent> blockInstructionsMap = new HashMap<>(); 
+		
+		return blockInstructionsMap;
+	}
+	protected static String generateIf(String input, Map<String, String> map, HashMap<String, InstructionContent> blockInstructionsMap) {
+		String blockName = input;
+		
+		List<String> instructions = new ArrayList<String>();
+		List<String> elseInstructions = new ArrayList<String>();
+		while(true){
+			input = scan.nextLine();
+			SimpleParser.isInstruction(input, map,  blockInstructionsMap);
+			if(input.equals("q")){
+				InstructionContent content = new InstructionContent(blockName, instructions);
+				blockInstructionsMap.put(blockName, content);
+				break;
+			}
+			if(input.equals("else:")){
+				while(true){
+					input = scan.nextLine();
+					SimpleParser.isInstruction(input, map, blockInstructionsMap);
+					if (input.equals("q")){
+						InstructionContent content = new InstructionContent(blockName,instructions, elseInstructions);
+						blockInstructionsMap.put(blockName, content);
+						break;
+					}
+					elseInstructions.add(input);
+				}
+				break;
+			}
+			instructions.add(input);
+			
+		}	
+		return input;
+	}
+
+	protected static void generateFunction(String input, HashMap<String, String> varMap, HashMap<String, InstructionContent> functionMap) {
+		    List<String> paramNames = new ArrayList<String>();
+			String[] tokens = Tokenizer.tokenize(input);
+			String funcName = tokens[0].replace("def ", "");
+			String[] varTokens = tokens[2].split(",");
+			int varCount = varTokens.length;
+			
+			for(int i = 0; i <varCount; i ++ ){
+				paramNames.add(varTokens[i].trim());
+			}
+			
+			
+		List<String> instructions = new ArrayList<String>();
+		
+		while(true){
+			input = scan.nextLine();
+			
+			SimpleParser.isInstruction(input, varMap, functionMap);
+			
+			if(input.equals("q")){
+				InstructionContent content = new InstructionContent(funcName, instructions, paramNames, varCount);
+				functionMap.put(funcName, content);
+				break;
+			}
+			
+			instructions.add(input);
+			
+		}
+		
+	}
+
+	protected static String generateForLoop(String input, Map<String, String> map, HashMap<String, InstructionContent> blockInstructionsMap) {
+		String blockName = input;
+		
+		List<String> instructions = new ArrayList<String>();
+		
+		while(true){
+			input = scan.nextLine();		
+			SimpleParser.isInstruction(input, map, blockInstructionsMap);		
+			if(input.equals("q")){
+				InstructionContent content = new InstructionContent(blockName, instructions);
+				blockInstructionsMap.put(blockName, content);
+				break;
+			}		
+			instructions.add(input);		
+		}
+	
+		
+		return input;
+	}
+	
+	private static void print(String input){
+		if(input != null){
+			System.out.println(input);
+		}
+		
+	}
+
+	private static boolean checkForVars(String input, Map<String, String> map, String[] tokens) {
 		for(int i = 0; i < tokens.length; i++){
-			if(tokens[i].matches(regexVar)){
-				if(isAssignment(input)){
+			if(tokens[i].matches(REGEXVAR)){
+				if(SimpleParser.isAssignment(input)){
 						String right = "";
 						String left = tokens[i];
-						if(checkForVars(arrayToString(tokens, i+2, tokens.length), map)){
-							String tmp = updateInput(input, map);
-							checkForVars(tmp, map);
+						String[] rightTokens;
+						if(SimpleParser.containsVars(tokens, i +2, tokens.length)){
+							/*Resolve all variables, the assign then evaluate them */
+							right = arrayToString(tokens,i+2, tokens.length);
+							rightTokens= Tokenizer.tokenize(right);
+							right = Evaluator.updateInput("", map, rightTokens);
+							right = Evaluator.eval(right);
 						}
 						else{
-							right = eval.eval(arrayToString(tokens, i+2, tokens.length));
+							right = Evaluator.eval(arrayToString(tokens, i+2, tokens.length));				
 						}
-						map.put(left, right);
-						
+						map.put(left,right);					
 						break;
-					
-					
 				}
 				else{
 					if(map.containsKey(tokens[i])){
-						String var = map.get(tokens[i]);
-						System.out.println(tokens[i] +" = " +var);
 						return true;
 					}
 					else{
 						System.out.printf("Var: %s is not saved\n", tokens[i]);
 						return false;
-					}
-					
+					}	
 				}
 			}
-		}
-		
+		}	
 		return false;
 	}
 
-	/**
-	 * Checks if the given statement is an assignment (e.g. 'a = 2')
-	 * @param input - given String
-	 * @return true if the given statement is an assignment, false if not
-	 */
-	private static boolean isAssignment(String input) {		
-		String[] tokens = input.split("=");
-		if(tokens.length == 2){
-			return true;
+	
+	protected static String arrayToString(String[] array, int start, int end){
+		StringBuilder builder = new StringBuilder();
+		for (int i =start; i < end; i++){
+			builder.append(array[i]);
 		}
-		
-		return false;
+		String str = builder.toString();
+		return str;	
 	}
-
 }
